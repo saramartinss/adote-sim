d6e5c5182fde913c9d61f2c7304127f3838b061c
sarinhamartins <sariinha.martins@gmail.com>
2018-12-04 14:04:30 -0200

    código para validação do formulário

---
+++ script/main.js
@@ -0,0 +1,37 @@
+let form = $('.contato__form');
+
+form.validate({
+    errorElement: 'span',
+    rules: {
+        name: {
+            required: true,
+            minlength: 6
+        },
+        email: {
+            required: true,
+            email: true
+        },
+        phone: {
+            required: true,
+            minlength: 10
+        },
+        message: {
+            required: true,
+        }
+    },
+    messages: {
+        name: {
+            required: 'Por favor, digite seu nome completo',
+            minlength: 'Por favor, digite seu nome completo'
+        },
+        email: {
+            required: 'Por favor, digite seu email',
+            email: 'Por favor, digite um e-mail válido'
+        },
+        phone: {
+            required: 'Por favor, digite seu telefone para contato',
+            minlength: 'Por favor, digite seu número para contato com ddd'
+        },
+        message: 'Por favor, digite sua mensagem'
+    }
+})
