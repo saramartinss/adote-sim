19821b2712f27c0260c1caaf2cc010b029c86105
sarinhamartins <sariinha.martins@gmail.com>
2018-12-05 15:14:32 -0200

    arquivos slick

--- script/main.js
+++ script/main.js
@@ -37,3 +37,25 @@ form.validate({
         }
     }
 })
+
+$(function () { 
+    $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+   
+});
+$('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    focusOnSelect: true,
+    arrows: true,
+    centerMode: true
+    
+});
+
+})
