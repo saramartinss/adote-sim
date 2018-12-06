e558d87eed7dca0abce627763e6632504099bb50
sarinhamartins <sariinha.martins@gmail.com>
2018-12-06 17:19:16 -0200

    Netlify com AJAX

--- script/main.js
+++ script/main.js
@@ -89,3 +89,19 @@ window.onclick = function(event) {
         viewAllModal.style.display = "none";
     }
 }
+
+
+$(".contato__form").submit(function(e) {
+    e.preventDefault();
+    console.log()
+    var $form = $(this);
+    $.ajax({
+        method: 'POST',
+        url: this.getAttribute('action'),
+        data: $form.serialize(),
+        success: function() {
+            alert('Foi')
+        }
+    });
+  });
+  
